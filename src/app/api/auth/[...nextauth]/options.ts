c5c4ec92d20262a82d4d2b6a94d2ac7fe8833bc1
HusainMansuri1 <mansurihusain99@gmail.com>
2024-05-31 20:29:11 +0530

    auth0 using custom app creds

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -5,15 +5,25 @@ const authOptions = {
   providers: [
     // !!! Should be stored in .env file.
     Auth0Provider({
-      clientId: `Be5vsLunFvpzPf4xfXtaMxrZUVBjjNPO`,
-      clientSecret: `08F9X84FvzpsimV16CQvlQuwJOlqk-GqQgEdcq_3xzrn1K3UHnTCcRgMCwBW7api`,
-      issuer: `https://dev-qg1ftdys736bk5i3.us.auth0.com`,
+      clientId: `FosOFa9i24lIP4fovVie6iqEwSk2XKYO`,
+      clientSecret: `cbgHB7B-a-pC1qn4lid2qjNRE0Mvg9_-QwMALMoQgSeqLs4jR-QddbJyU8Kvj0Pf`,
+      issuer: `https://dev-v61voas4v4g2h3o0.us.auth0.com`,
     }),
   ],
   secret: `UItTuD1HcGXIj8ZfHUswhYdNd40Lc325R8VlxQPUoR0=`,
-  // authorizationParams:{
-  //   redirect_uri: window.location.origin
-  //   }
+//   callbacks: {
+//     async redirect(url:any, baseUrl:any) {
+//         // Redirect to the base URL if the URL is unsafe
+        
+//         if (url.url.startsWith(url.baseUrl)) {
+//             return url.url;
+//         } else if (url.url.startsWith('/')) {
+//             return new URL(url.url, url.baseUrl).toString();
+//         }
+//         return url.baseUrl;
+//     },
+ 
+// },
 };
 
 export default authOptions;
